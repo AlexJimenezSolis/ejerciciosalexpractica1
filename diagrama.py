dcd14aa3859dc3ffd2e18d0b85b437f1a9d3dfd9
AlexJimenezSolis <117433023+AlexJimenezSolis@users.noreply.github.com>
2022-11-09 22:32:55 +0100

    Add files via upload

---
+++ diagrama.py
@@ -0,0 +1,26 @@
+print("quiero un cafe")
+accion_uno = float(input("tengo el cafe hecho : "))
+
+s = 1
+n = 0
+
+if accion_uno == s:
+    print("calientalo")
+
+    accion_dos = float(input("esta dulçe el cafe? :" ))
+
+    if accion_dos == s:
+        print("ok tomate el cafe")
+
+
+if accion_uno == n:
+    print("hazte uno")
+
+    accion_dos = float(input("esta dulçe el cafe? :"))
+
+    if accion_dos == n:
+        print("ponle azucar")
+
+
+
+
---
+++ operacionesmatematicas.py
@@ -0,0 +1,52 @@
+x= 5
+y= 2
+
+float_resta=x-y
+print(float_resta)
+
+x= 5
+y= 2
+
+float_suma=x+y
+print(float_suma)
+
+x= 5
+y= 2
+
+float_suma=x**y
+print(float_suma)
+
+x= 5
+x+=1
+
+
+float_incrementa=x-y
+print(x)
+
+x= 5
+x-=2
+
+
+float_=x-y
+print(x)
+
+
+
+x=5
+y=5
+float_modul=x%y
+print(float_modul)
+
+x=8
+y=10
+
+float_divisionentrea=8/10
+print(float_divisionentrea)
+
+x=100
+y=3
+
+float_division=x//y
+print(float_division)
+
+
