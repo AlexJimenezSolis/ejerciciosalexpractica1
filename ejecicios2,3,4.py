6cf418ac7cd7241544401c75e1d05d01098ead8d
AlexJimenezSolis <117433023+AlexJimenezSolis@users.noreply.github.com>
2022-11-09 22:32:19 +0100

    Add files via upload

---
+++ ejecicios2,3,4.py
@@ -0,0 +1,21 @@
+x="alexjimenezsolis"
+
+print(x)
+
+edad_julia=15
+edad_adulta=18
+
+float_edades = (edad_julia>edad_adulta)
+
+print(float_edades)
+
+nota = float(input("nota del alumno : "))
+if nota < 5:
+  print("para recuperaciones")
+if nota >= 5 and nota <= 6:
+  print("Bé")
+if nota > 6 and nota <= 8:
+  print("Notable")
+if nota > 8:
+  print("exelente")
+
